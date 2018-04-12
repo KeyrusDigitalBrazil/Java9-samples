3eefc3e6e172c637727d5ae54ffd0501ddc1dc5f
Renã Souza <rena.souza@keyrus.com.br>
2018-04-11 22:20:45 -0300

    generating JRE immage

--- compile.sh
+++ compile.sh
@@ -5,3 +5,12 @@ javac -d mods/br.com.keyrus.commerce.front  --module-path mods src/br.com.keyrus
 
 
 javac -d mods/br.com.keyrus.commerce.front  --module-path mods src/br.com.keyrus.commerce.front/module-info.java $(find src/br.com.keyrus.commerce.front -name "*.java")
+
+ java --module-path mods -m br.com.keyrus.commerce.front/br.com.keyrus.commerce.front.Main
+
+
+
+### GENERATE SMALL JRE
+ jlink --module-path /usr/lib/jvm/java-9-oracle/jmods:mods --add-modules br.com.keyrus.commerce.front --output small-jre
+
+ ./small-jre/bin/java -m br.com.keyrus.commerce.front/br.com.keyrus.commerce.front.Main
